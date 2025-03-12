73131ea19360428ce29f0ae8c295145a3c92879a
hebehh <hebehh@gmail.com>
2025-03-13 00:20:13 +0800

    nicer by shebecoding

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ rightContent }) => {
   return (
     <div className="flex justify-between items-center mb-8">
-      <div className="flex items-center">
+      <div className="flex items-end">
         <Link href="/" className="text-2xl font-bold text-white hover:text-violet-100 transition-colors">
           s<span className="text-violet-300">hebe</span>testing
         </Link>
@@ -16,9 +16,10 @@ export const Header: React.FC<HeaderProps> = ({ rightContent }) => {
           href="https://shebecoding.com" 
           target="_blank" 
           rel="noopener noreferrer"
-          className="ml-2 text-sm italic text-gray-300 hover:text-violet-200 transition-colors"
+          className="ml-1 text-sm italic text-gray-300 hover:opacity-80 transition-colors flex items-end"
         >
-          by s<span className="text-violet-300">hebe</span>coding
+          <span className="text-gray-300 pr-1 ">by</span>
+          <span className="text-[#81d8df]">s<span className="text-[#effcfc]">hebe</span>coding</span>
         </Link>
       </div>
       {rightContent}
